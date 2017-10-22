9785373821602515959a5258c3b5246aa31ea22d
gokceuludogan <gokceuludogan@gmail.com>
2017-10-22 20:42:28 +0300

    Add a basic timeline

---
+++ app/modules/timeline/timeline.controller.js
@@ -0,0 +1,16 @@
+(function()
+{
+    angular
+        .module("interestHub")
+        .controller("TimelineCtrl", TimelineCtrl);
+    
+    function TimelineCtrl($scope, $uibModalInstance, modalFactory) {
+
+
+    	
+		    	
+ 
+	}
+
+
+})();
