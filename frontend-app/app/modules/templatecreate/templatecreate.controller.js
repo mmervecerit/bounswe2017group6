237210f099b4786919acc52d9198c43103e22ca0
ymanav <yusufcanmanav@gmail.com>
2017-11-02 04:33:31 +0300

    Template Creation Page

--- frontend-app/app/modules/templatecreate/templatecreate.controller.js
+++ frontend-app/app/modules/templatecreate/templatecreate.controller.js
@@ -6,6 +6,21 @@
     
     function TemplateCreateCtrl($scope,  $rootScope, $location)
     {
+		$scope.choices = [{id: '1'}];
+  
+  $scope.addNewChoice = function() {
+    var newItemNo = $scope.choices.length+1;
+    $scope.choices.push({'id':newItemNo});
+  };
+    
+  $scope.removeChoice = function(item) {
+	var index = $scope.choices.indexOf(item);
+	 $scope.choices.splice(index, 1);
+	 for(i=index;i<$scope.choices.length;i++) { 
+		$scope.choices[i].id=$scope.choices[i].id-1;
+	}
+	 
+  };
 
         
       
