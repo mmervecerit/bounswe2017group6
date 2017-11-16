c852ca1b0f7d0ecc397be89408ba5bed94bb2c88
ymanav <yusufcanmanav@gmail.com>
2017-11-16 11:41:44 +0300

    Content creation with custom templates

--- frontend-app/app/modules/post/post.controller.js
+++ frontend-app/app/modules/post/post.controller.js
@@ -5,7 +5,7 @@
         .module("interestHub")
         .controller("PostCtrl", PostCtrl);
     
-    function PostCtrl($scope,  $rootScope, $location, PostService, $routeParams)
+    function PostCtrl($scope,  $rootScope, $location, PostService, $routeParams, TemplateService)
 
     {    
 
@@ -14,13 +14,16 @@
         $scope.update = update;
         $scope.add    = add;
       	$scope.tab = {};
+		$scope.typeSelect=typeSelect;
         function init() {
        
 
             PostService
                 .getAllPosts($routeParams.id)
                 .then(handleSuccess, handleError);
-
+			TemplateService
+				.getAllTemplates($routeParams.id)
+				.then(tempSuccess, tempError);
         }
         
         init();
@@ -44,6 +47,7 @@
             PostService
                 .updatePost(post._id, post)
                 .then(handleSuccess, handleError);
+			
         }
         /*
         function add(post)
@@ -55,25 +59,43 @@
 
             console.log("added");
         } */     
-		var componentList;
+		$scope.content={};
 		function add(content)
         {
-			
-			if(content.long_text!=null){
-				componentList={"owner_id":1,"content_type_id":1, "components":[{ "component_type": "text", "order": 1, "small_text": null, "long_text":content.long_text , "url": "" }]};
-  
-			/*var component = {component_type:"text", order:1, long_text:content.text};
-				ContentService
-					.createComponent(component)
-					.then(handleSuccess, handleError);
-			*/
-			//console.log(componentList);
-			
-            PostService
-                .createPost($rootScope.group.id, componentList)
-                .then(handleSuccess, handleError);
+			$scope.req={"content_type":{},"owner":{
+            "url": "http://34.209.230.231:8000/users/1/",
+            "id": 1,
+            "username": "admin",
+            "email": "admin@interesthub.com"
+			},"owner_id": 1,"content_type_id": $scope.content.content_type_id,"components":[]}
+			console.log($scope.content);
+			for(i=0;i<$scope.templates.length;i++){
+				if($scope.templates[i].id==$scope.content.content_type_id){
+					$scope.con_type=angular.copy($scope.templates[i]);
+				}
+			}
+			for(i=0;i<$scope.con_type.components.length;i++){
+				temp= {
+                "component_type": $scope.con_type.components[i],
+                "order": i+1,
+                "type_data": {
+                    "data": content.comps[$scope.con_type.component_names[i]]
+                }
 				
             }
+			$scope.req.components.push(temp);
+			}
+			$scope.req.content_type=$scope.con_type;
+			
+			console.log($scope.req)
+			
+	
+			
+            PostService
+                .createPost($routeParams.id, $scope.req)
+                .then(handleSuccess, handleError);
+				
+            
 
             
         }
@@ -88,11 +110,27 @@
             $scope.error = error;
             //console.log(error);
         }
+		function tempSuccess(response) {
+			$scope.templates=response.data;
+			console.log($scope.templates);
+			
+			
+
+        }
+		$scope.content={owner_id:'1',content_type_id:'',comps:[]};
+		function typeSelect(type_id){
+			$scope.content={owner_id:'1',content_type_id:'',comps:[]};
+			$scope.content.content_type_id=type_id;
+			//console.log($scope.content);
+			
+		}
+
+        function tempError(error) {
+            $scope.temperror = error;
+            //console.log(temperror);
+        }
 		
-		$scope.tabs = [
-		    { title:'Dynamic Title 1', content:'Dynamic content 1' },
-		    { title:'Dynamic Title 2', content:'Dynamic content 2' }
-		  ];
+		$scope.templates = [ ];
 
 
 		  $scope.model = {
