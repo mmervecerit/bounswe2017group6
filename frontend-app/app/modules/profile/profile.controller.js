9ed36524a6b729e2db4da5f1c598dd4860f2d57f
gokceuludogan <gokceuludogan@gmail.com>
2017-11-17 23:25:31 +0200

    Add adding and removing interest and editing user functions

--- frontend-app/app/modules/profile/profile.controller.js
+++ frontend-app/app/modules/profile/profile.controller.js
@@ -6,13 +6,24 @@
     
     function ProfileCtrl($scope,  $rootScope, $routeParams, $location, UserService)
     {
+        $scope.removeInterest = removeInterest;
+        $scope.addInterest = addInterest;
+        $scope.editUser = editUser;
     	function init(){
+
     		UserService.getUser($routeParams.id)
     			.then(handleSuccess, handleError);
     	}
 
         init();
-		console.log($scope.user);      	
+		$scope.user = new Object();
+        $scope.user.firstname="Gökçe";
+        $scope.user.lastname="Uludoğan";
+        $scope.user.gender="Female";
+        $scope.user.birthdate="1995-06-28";
+        $scope.interests = ["data science","robotics","artificial intelligence","science fiction", "travel","cycling"];
+        console.log($scope.user);     
+        
       	function handleSuccess(response) {
             $scope.user = response.data;
         	
@@ -20,6 +31,19 @@
 
         function handleError(error) {
             $scope.error = error;
+        }
+
+        function removeInterest($index){
+            $scope.interests.splice($index,1);
+        }
+        function addInterest(interest){
+            $scope.interests.push(interest);
+            $scope.interest = "";
+
+        }
+        function editUser(user){
+            UserService.updateUser(user)
+                .then(handleSuccess,handleError);
         }
  
 	   }
