9dff08ce003fd624b51269cd454f0c0282ff2859
mmervecerit <mmervecerit@gmail.com>
2017-11-22 21:51:28 +0300

    User Profile Fragments are created and added, few minor issues are solved.

--- android-app/app/src/main/java/com/cmpe451/interesthub/adapters/UserProfileTabsAdapter.java
+++ android-app/app/src/main/java/com/cmpe451/interesthub/adapters/UserProfileTabsAdapter.java
@@ -3,12 +3,15 @@ package com.cmpe451.interesthub.adapters;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.util.Log;
+
 
 import com.cmpe451.interesthub.fragments.UserEvents;
 import com.cmpe451.interesthub.fragments.UserGroups;
 import com.cmpe451.interesthub.fragments.UserProfile;
 import com.cmpe451.interesthub.fragments.UserTimelineFragment;
+import com.cmpe451.interesthub.fragments.User_Followers_Fragment;
+import com.cmpe451.interesthub.fragments.User_Following_Fragment;
+import com.cmpe451.interesthub.fragments.User_MyPosts_Fragment;
 
 /**
  * Created by eren on 19.10.2017.
@@ -20,13 +23,11 @@ public class UserProfileTabsAdapter extends FragmentStatePagerAdapter {
     public UserProfileTabsAdapter(FragmentManager fm){
         super(fm);
     }
-
-
     @Override
     public Fragment getItem(int position) {
-
-            return new UserEvents();
-
+        if(position==0) return new User_MyPosts_Fragment();
+        else if (position==1) return new User_Followers_Fragment();
+        else return new User_Following_Fragment();
     }
 
     @Override
